e2d17487f932892ae617a7c2b5e2ed6cc24e4b08
PaulaBere <106834529+PaulaBere@users.noreply.github.com>
2023-11-08 11:15:02 -0300

    Tercer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HomeModule } from './home/home.module';
+import { RecetasComponent } from './home/recetas/recetas.component';
 
 @NgModule({
   declarations: [
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-import { HomePageComponent } from './home-page/home-page.component';
+import { RecetasComponent } from './recetas/recetas.component';
 import { ItemRecetaComponent } from './recetas/lista-recetas/item-receta/item-receta.component';
 import { DetalleRecetaComponent } from './recetas/detalle-receta/detalle-receta.component';
-import { ListaRecetasComponent } from './recetas/lista-recetas/lista-recetas.component';
-import { ListaComprasComponent } from './lista-compras/lista-compras.component';
 import { EditarCompraComponent } from './lista-compras/editar-compra/editar-compra.component';
-import { RecetasComponent } from './recetas/recetas.component';
 import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ListaComprasComponent } from './lista-compras/lista-compras.component';
+import { ListaRecetasComponent } from './recetas/lista-recetas/lista-recetas.component';
 
 
 @NgModule({
@@ -25,7 +25,7 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
   ]
 })
 export class HomeModule { }
