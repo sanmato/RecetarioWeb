a73d1b088ee230922729d844d8ef540ae26d7e93
PaulaBere <106834529+PaulaBere@users.noreply.github.com>
2023-11-13 18:11:48 -0300

    Prueba de estilo nuevo

--- src/app/home/recetas/servicio-recetas.service.ts
+++ src/app/home/recetas/servicio-recetas.service.ts
@@ -10,9 +10,27 @@ export class ServicioRecetasService {
   recetas: Receta[] = [
     new Receta(
       'Omelette',
-      'Una prueba para ver',
+      'Receta de Prueba',
       'https://www.paulinacocina.net/wp-content/uploads/2019/09/okokokok.jpg',
       [new Ingrediente('Huevo', 2, 'unidades'), new Ingrediente('Queso', 20, 'grs')]
+    ),
+    new Receta(
+      'Calabaza Rellena',
+      'Otra prueba para ver',
+      'https://www.clara.es/medio/2023/01/08/receta-calabaza-rellena-bolonesa_c46f79f3_1280x853.jpg',
+      [new Ingrediente('Calabaza', 1, 'unidad'), new Ingrediente('Carne', 200, 'grs')]
+    ),
+    new Receta(
+      'Spaguetti con bolognesa',
+      'Otra prueba para ver',
+      'https://img-global.cpcdn.com/recipes/f4602e2b1a90452c/400x400cq70/photo.jpg',
+      [new Ingrediente('Calabaza', 1, 'unidad'), new Ingrediente('Carne', 200, 'grs')]
+    ),
+    new Receta(
+      'Milanesa con papas fritas',
+      'Otra prueba para ver',
+      'https://vinomanos.com/wp-content/uploads/2019/02/milanesas-receta.jpg',
+      [new Ingrediente('Calabaza', 1, 'unidad'), new Ingrediente('Carne', 200, 'grs')]
     ),
     new Receta(
       'Calabaza Rellena',
